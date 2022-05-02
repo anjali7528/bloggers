87d4a75ee9e7698e76a7d55a767da2b2d65b9f2f
anjali7528 <anjaligandhi248@gmail.com>
2022-05-02 23:28:20 +0530

    new commit

--- src/post/Post.js
+++ src/post/Post.js
@@ -3,6 +3,6 @@ import './post.css'
 
 export default function Post() {
   return (
-    <div className='post'>Post</div>
+    <div className='post'></div>
   )
 }
--- src/sidebar/Sidebar.js
+++ src/sidebar/Sidebar.js
@@ -3,7 +3,13 @@ import "./sidebar.css";
 
 function Sidebar() {
   return (
-    <div className='sidebar'>Sidebar</div>
+    <div className='sidebar'>
+    <div className='sidebarItem'>
+      <span className='sidebarTitle'>ABOUT ME</span>
+      <img src="https://images.unsplash.com/photo-1480406266260-49c193257b6d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80" alt=""></img>
+      <p>Consequat eiusmod quis veniam duis id amet dolore enim ut laborum occaecat cupidatat laboris fugiat.</p>
+    </div-div>'.'
+    </div>
   )
 }
 
